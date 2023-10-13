1c04281c28c354af958c8c63d15085aad147cfe1
David Atanda <ololadedavid15@gmail.com>
2023-10-13 22:51:42 +0100

    set display name

---
+++ src/components/DisplayName.js
@@ -0,0 +1,38 @@
+import { ArrowBack } from '@mui/icons-material';
+import { Button, IconButton } from '@mui/material';
+import React, { useState, useEffect, Fragment } from 'react';
+import { useNavigate } from 'react-router';
+
+const SetDisplayName = ()=>{
+    const navigate = useNavigate()
+    return (
+        <Fragment>
+            <div className='py-5'>
+                <IconButton onClick={()=>navigate('/')} className='text-green pb-3'>
+                    <ArrowBack /> back
+                </IconButton>
+                <div className='d-flex justify-content-center'>
+                    <div className='col-md-6 col-xl-4'>
+                        <p className='text-center h2 fw-bold'>
+                            Set Display Name
+                        </p>
+                        <p className='text-center'>
+                            Your Display Name is how you want to be known in the Plebitask community
+                        </p>
+                        <div className='form-group'>
+                            <p className='text-green text-center'>
+                                Input Display Name
+                            </p>
+                            <input className='form-control rounded-0 plebitask-input py-3' />
+                            <Button className='px-4 py-2 btn-plebitask text-white my-4 w-100 fw-bold'>
+                                NEXT
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default SetDisplayName
---
+++ src/pages/SetPage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import NavbarBrand from '../assets/navbar-brand.png'
+import { Outlet } from 'react-router';
+
+const SetPage = ()=>{
+    return (
+        <Fragment>
+            <div className='set-my-page p-5'>
+                <div className='d-flex justify-content-between'>
+                    <div>
+                        <img  src={NavbarBrand} className="navbar-brand img-fluid"/>
+                    </div>
+                    <div>
+                        <p>
+                            Plebitask Technology  - &copy; 2023  All rights reserved.
+                        </p>
+                    </div>
+                </div>
+                <Outlet />
+            </div>
+        </Fragment>
+    )
+}
+
+export default SetPage
