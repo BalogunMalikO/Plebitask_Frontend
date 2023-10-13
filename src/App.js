648e8b9e102030096dc183b561407123f0692da2
David Atanda <ololadedavid15@gmail.com>
2023-10-13 22:51:58 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './pages/Login';
+import Connect from './pages/Connect';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SetPage from './pages/SetPage';
+import SetDisplayName from './components/DisplayName';
+import SetDisplayPicture from './components/DisplayPicture';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Connect />} />
+          <Route path='/set' element={<SetPage />}>
+            <Route path='/set/display-name' element={<SetDisplayName />} />
+            <Route path='/set/display-picture' element={<SetDisplayPicture />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
