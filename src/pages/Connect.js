e8b1ec3928d69faa49db822ef1c6b921a236a7c2
David Atanda <ololadedavid15@gmail.com>
2023-10-15 06:05:54 +0100

    feat: users can now connect wallet

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -1,25 +1,66 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import NavbarBrand from '../assets/navbar-brand.png'
 import Rafiki from '../assets/rafiki.png'
-import { Button } from '@mui/material';
+import { Alert, Button, Snackbar } from '@mui/material';
+import { useNavigate } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
 const Login = ()=>{
+    const navigate = useNavigate()
+    const [snackbarstatus, setSnackbarStatus] = useState(false)
+    const [response, setResponse] = useState('')
+    const connect = async()=>{
+        try {
+            if(typeof window.liquid !== 'undefined') {
+              window.liquid.enable().then(()=>{
+                window.liquid.getAddress().then(res=>{                    
+                    sessionStorage.setItem('liquid_addr', JSON.stringify(res))
+                    navigate('/set/display-name')
+                  }).catch(err=>{
+                    setResponse('Unable to retrieve address')             
+                    setSnackbarStatus(true)
+                })
+              }).catch(err=>{
+                setResponse('User rejected')             
+                setSnackbarStatus(true)
+              })                                  
+            }else {
+                setResponse('No Liquid Provider Available')             
+                setSnackbarStatus(true)
+            }
+          }
+          catch(error) {
+            setResponse('User denied permission or cancelled')             
+            setSnackbarStatus(true)
+          }
+    }
+    const handleClose = (event, reason)=>{
+        if (reason == 'clickaway') {
+            return;
+        }
+
+        setSnackbarStatus(false)
+
+    }
+
     return (
         <Fragment>
             <div className='connect'>                
                 <div className='row w-100 mx-auto'>
                     <div className='col-md-7 bg-plebitask px-5'>
                         <div className='py-5'>
-                            <img  src={NavbarBrand} className="navbar-brand img-fluid"/>
+                            <NavLink className={'navbar-brand'} to={'/'}>
+                                <img  src={NavbarBrand} className="img-fluid"/>
+                            </NavLink>
                         </div>
-                        <div className='py-5 col-xl-6 mt-4'>
+                        <div className='py-5 col-xl-7 mt-4'>
                             <p className='fw-bold fs-2 pt-5'>
                                 Welcome to Plebitask
                             </p>
                             <p className='fw-light fs-3'>
                             Connect your Wallet to continue.
                             </p>
-                            <Button className='px-4 py-2 btn-plebitask text-white mt-1 mb-5 fw-bold'>
+                            <Button onClick={connect} className='px-4 py-2 btn-plebitask text-white mt-1 mb-5 fw-bold'>
                                 Connect Wallet
                             </Button>
                         </div>
@@ -40,6 +81,12 @@ const Login = ()=>{
                     </div>
                 </div>                
             </div>
+
+            <Snackbar open={snackbarstatus} onClose={handleClose} autoHideDuration={4000} >
+                <Alert severity={'error'} onClose={handleClose} className='font-poppins' >
+                    {response}
+                </Alert>
+            </Snackbar>
         </Fragment>
     )
 }
