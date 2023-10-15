82b6ad15f0af2043f0db64251f6a158bb0dc51a4
David Atanda <ololadedavid15@gmail.com>
2023-10-15 06:04:40 +0100

    chore: app changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SetPage from './pages/SetPage';
 import SetDisplayName from './components/DisplayName';
 import SetDisplayPicture from './components/DisplayPicture';
+import User from './pages/User';
+import Dashboard from './components/Dashboard';
+import About from './components/About';
 
 function App() {
   return (
@@ -16,6 +19,10 @@ function App() {
           <Route path='/set' element={<SetPage />}>
             <Route path='/set/display-name' element={<SetDisplayName />} />
             <Route path='/set/display-picture' element={<SetDisplayPicture />} />
+          </Route>
+          <Route path='/user' element={<User />} >
+            <Route path='/user/dashboard' element={<Dashboard />} />
+            <Route  path='/user/about' element={<About />} />
           </Route>
         </Routes>
       </BrowserRouter>
